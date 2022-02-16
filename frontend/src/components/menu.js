d69a9a9cb35c9ab0c76a28a70ae33300ecd55a83
BrianMcLucky <volker415@gmail.com>
2022-02-16 22:05:02 +0300

    lesson2

---
+++ frontend/src/components/menu.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Menu () {
+    return (
+        <nav className="menu">
+            <ul>
+                <li><a href="#">ToDo</a></li>
+                <li><a href="#">Проект</a></li>
+                <li><a href="#">Авторизация</a></li>
+            </ul>
+        </nav>
+    )
+}
